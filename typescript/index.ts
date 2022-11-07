f325cad721584e6ee301826991b262da9865507c
Muhammed Abiola <88386924+msrspsr@users.noreply.github.com>
2022-11-07 03:51:08 +0100

    bubble sort

--- typescript/index.ts
+++ typescript/index.ts
@@ -27,3 +27,15 @@ function binarySearch(haystack: number[], needle: number): boolean {
 
 	return false;
 }
+
+function bubbleSort(arr: number[]): void {
+	for (let i = 0; i < arr.length; i++) {
+		for (j = 0; j < arr.length - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				const tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
