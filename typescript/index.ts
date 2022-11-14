a53c40c2db111637957b5098d1784d7d0a5c5bdc
Muhammed Abiola <88386924+msrspsr@users.noreply.github.com>
2022-11-14 03:41:57 +0100

    linked lists interface

--- typescript/index.ts
+++ typescript/index.ts
@@ -39,3 +39,14 @@ function bubbleSort(arr: number[]): void {
 		}
 	}
 }
+
+interface LinkedList<T> {
+	get length(): number;
+	insertAt(item: T, index: number): void;
+	remove(item: T): T | undefined;
+	removeAt(index: number): T | undefined;
+	append(item: T): void;
+	prepend(item: T): void;
+	get(index: number): T | undefined;
+}
+
