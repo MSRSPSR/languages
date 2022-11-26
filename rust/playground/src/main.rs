5e6eccceaf9997b770e3475fb9a248d7fa9e8e02
mrxvt <113499610+mrxvt@users.noreply.github.com>
2022-11-26 14:58:29 +0100

    guessing game

--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -1,30 +1,16 @@
-fn main() {
-    let x = 5;
-    // x = 6; -- immutable
+use std::io;
 
-    let mut y = 10;
-    y = 5;
+fn main()
+{
+    println!("Guess the number!");
+    println!("Please input your guess.");
 
-    // i8, u8, i16, u16, i32, u32, i64, u64, isize, usize
-    // f32, f64
-    
-    let mut z: u32 = x + y;
-    z += x * y;
-    z -= x - y;
-    z /= x / y;
-    z = x % y;
+    let mut guess = String::new();
 
-    // bool: true/false
-    let c = 'c';
-    let d: char = 'd';
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    // Tuples
-    let t: (i32, f64, char) = (42, 6.12, 'j');
-    let (n, m, k) = t;
-    let (_, _, f) = t;
-    t.0; t.1; t.2;
+    println!("You guessed {}", guess);
 
-    // Arrays
-    let arr = [1, 2, 3, 4, 5];
-		let a0 = arr[0];
 }
