643fb247d321f1c14258ee9cac862e2e9ad27d2e
mrxvt <113499610+mrxvt@users.noreply.github.com>
2022-12-31 06:52:23 +0100

    feat: termperature conversion

---
+++ c/kandr/solutions/1-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/* 
+	* printf outputs a the character that comes after a backslash
+	* if no escape sequence is found
+	*/
+
+	printf("\c");
+	return 0;
+}
