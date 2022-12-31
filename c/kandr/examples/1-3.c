88432b6ac6205ea334ad8e6713db3bb70ffc30a9
mrxvt <113499610+mrxvt@users.noreply.github.com>
2022-12-31 14:48:59 +0100

    style: add newline to output

--- c/kandr/examples/1-3.c
+++ c/kandr/examples/1-3.c
@@ -5,7 +5,7 @@ int main(void)
 	int fahr;
 
 	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-		printf("%3d\t%6.2f", fahr, (5.0 / 9.0) * (fahr - 32));
+		printf("%3d\t%6.2f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 
 	return 0;
 }
