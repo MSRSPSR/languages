bb4532f422f32a3f7c6d59c4718d95916440f923
mrxvt <113499610+mrxvt@users.noreply.github.com>
2022-12-31 06:53:19 +0100

    style: right justify output

--- c/kandr/examples/fahr_celsius.c
+++ c/kandr/examples/fahr_celsius.c
@@ -16,7 +16,7 @@ int main(void)
 	while (fahr <= upper)
 	{
 		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		printf("%3d\t%6d\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 
